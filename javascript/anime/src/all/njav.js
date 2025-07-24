8fe87ce7da2a77889c51935c5313bfbef7bacdd2
cgcnl <cgcnl@localhost.local>
2025-07-24 17:33:44 +0700

    bump version

--- javascript/anime/src/all/njav.js
+++ javascript/anime/src/all/njav.js
@@ -8,7 +8,7 @@ const mangayomiSources = [{
     "isManga": false,
     "itemType": 1,
     "isNsfw": true,
-    "version": "0.1.0",
+    "version": "0.1.1",
     "apiUrl": "",
     "dateFormat": "",
     "dateFormatLocale": "",
